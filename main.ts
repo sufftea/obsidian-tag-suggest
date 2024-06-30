61b6e9f13091edff3aec05d07a8816164ddee8e4
alex <khorunzhiyal@gmail.com>
2024-06-30 22:59:21 +0300

    v1.0

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { match } from 'assert';
+import { Command, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Menu, EditorSuggest, EditorPosition, EditorSuggestTriggerInfo, TFile, EditorSuggestContext, getAllTags } from 'obsidian';
+import { __awaiter } from 'tslib';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,72 +12,137 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+
+class TagSuggesEntry {
+	constructor(
+		public tag: string,
+	) { }
+}
+
+
+
+class TagSuggest extends EditorSuggest<TagSuggesEntry> {
+	private plugin: MyPlugin;
+
+	constructor(plugin: MyPlugin) {
+		super(plugin.app);
+		this.plugin = plugin;
+	}
+
+	onTrigger(cursor: EditorPosition, editor: Editor, file: TFile | null): EditorSuggestTriggerInfo | null {
+		let line = editor.getLine(cursor.line).substring(0, cursor.ch);
+		let tag = line.match(/@\w*/)?.last();
+
+		if (tag) {
+			let test: EditorPosition = {
+				ch: 1,
+				line: 1,
+			};
+
+			return {
+				start: {
+					ch: line.lastIndexOf('@'),
+					line: cursor.line,
+				},
+				end: cursor,
+				query: tag.substring(1),
+			};
+		}
+
+		return null;
+	}
+
+	_isSubsequence(s: string, sub: string): boolean {
+		let i = 0, j = 0;
+		while (i < s.length && j < sub.length) {
+			if (s[i] === sub[j]) {
+				j++;
+			}
+			i++;
+		}
+		return j === sub.length;
+	}
+
+	async getFileTags(file: TFile) {
+		let cache = this.plugin.app.metadataCache.getFileCache(file);
+
+		if (cache == undefined) {
+			return [];
+		}
+
+		const tags = getAllTags(cache) ?? [];
+
+		tags.forEach((tag: string, index) => {
+			tags[index] = tag.replace("#", "");
+		});
+
+		return tags;
+	}
+
+	async getSuggestions(context: EditorSuggestContext): Promise<TagSuggesEntry[]> {
+		let currTags = context.editor
+			.getValue()
+			.match(/#[^ !@#$%^&*(),.?":{}|<>]*/g)
+			?.map((v) => v.substring(1)) ?? [];
+
+		const ARRAY_SIZE = 10;
+		let tagCount = new Map<string, number[]>();
+		let files = context.file.vault.getMarkdownFiles().filter((file) => {
+			return file != context.file;
+		});
+
+
+		let fileTags = await Promise.all(files.map((file) => this.getFileTags(file)));
+
+		for (let tags of fileTags) {
+			let score = tags?.filter((t) => currTags?.includes(t)).length;
+			if (score === undefined) {
+				continue
+			}
+			score = Math.min(score, ARRAY_SIZE);
+			for (let tag of tags ?? []) {
+				let curr = tagCount.get(tag) ?? Array(ARRAY_SIZE).fill(0);
+				curr[score]++;
+				tagCount.set(tag, curr);
+			}
+		}
+
+
+
+		return Array.from(tagCount.keys())
+			.sort(function (a, b) {
+				let aScore = tagCount.get(a)!;
+				let bScore = tagCount.get(b)!;
+				for (let i = ARRAY_SIZE; i >= 0; i--) {
+					if (aScore[i] == bScore[i]) {
+						continue;
+					}
+					return bScore[i] - aScore[i];
+				}
+				return 0;
+			})
+			.filter((v) => this._isSubsequence(v, context.query) && !currTags?.contains(v))
+			.map((v) => new TagSuggesEntry(v));
+	}
+
+	renderSuggestion(value: TagSuggesEntry, el: HTMLElement): void {
+		el.createEl('div', { text: `${value.tag}` })
+	}
+
+	selectSuggestion(value: TagSuggesEntry, evt: MouseEvent | KeyboardEvent): void {
+		this.context?.editor.replaceRange(`#${value.tag} `, this.context.start, this.context.end);
+	}
+
+
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEditorSuggest(new TagSuggest(this));
 	}
 
 	onunload() {
@@ -88,47 +155,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
